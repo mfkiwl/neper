7ae5b8cce2793cf2d34c5f5097e0e412a8d31191
Romain QUEY <quey@emse.fr>
2023-10-03 09:52:39 +0200

    Remove net_tess_opt_init_sset_weight
    Removed as unused.

--- src/neper_t/net_tess/net_tess_opt/net_tess_opt_init/net_tess_opt_init_sset/net_tess_opt_init_sset1.c
+++ src/neper_t/net_tess/net_tess_opt/net_tess_opt_init/net_tess_opt_init_sset/net_tess_opt_init_sset1.c
@@ -22,12 +22,8 @@ net_tess_opt_init_sset (struct IN_T In, int level, struct MTESS MTess,
   net_tess_opt_init_sset_pre (In, level, MTess, Tess, dtess, dcell, &var,
                               &pos, &weightexpr, &cooexpr, pTOpt);
 
-  if (!strcmp ((*pTOpt).optitype, "morpho"))
-    net_tess_opt_init_sset_weight (MTess, Tess, dtess, dcell, *pTOpt, var, pos,
-                                   weightexpr, rad);
-  else if (!strcmp ((*pTOpt).optitype, "ori"))
-    net_tess_opt_init_sset_rweight (MTess, Tess, dtess, dcell, *pTOpt, var, pos,
-                                    weightexpr, rad);
+  net_tess_opt_init_sset_weight (MTess, Tess, dtess, dcell, *pTOpt, var, pos,
+                                 weightexpr, rad);
 
   net_tess_opt_init_sset_multiseed (In, level, *pTOpt, var, rad, qty);
 
